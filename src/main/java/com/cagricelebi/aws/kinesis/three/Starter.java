2aa3564c9e599f3f8a3bd021f7f9081c78645b1e
az3 <azureel@gmail.com>
2016-12-02 13:26:42 +0300

    Added WorkerIdentifier metric. KCL dependency updated to v1.7.2. Updated properties file; prometheusPort added, workerId renamed.

--- src/main/java/com/cagricelebi/aws/kinesis/three/Starter.java
+++ src/main/java/com/cagricelebi/aws/kinesis/three/Starter.java
@@ -5,12 +5,16 @@ import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.kinesis.clientlibrary.lib.worker.KinesisClientLibConfiguration;
 import com.amazonaws.services.kinesis.clientlibrary.lib.worker.Worker;
+import com.amazonaws.services.kinesis.leases.impl.LeaseCoordinator;
+import com.amazonaws.services.kinesis.metrics.impl.MetricsHelper;
 import com.cagricelebi.aws.kinesis.three.metric.PrometheusRunnable;
+import com.google.common.collect.ImmutableSet;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.math.BigInteger;
 import java.security.SecureRandom;
 import java.util.Properties;
+import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import org.slf4j.Logger;
@@ -28,7 +32,7 @@ public class Starter {
     private Worker kclWorker;
 
     private ExecutorService prometheusExecutor;
-    private static final int prometheusPort = 8090;
+    private int prometheusPort;
     private String prometheusShutdownKey; // shutdown key can be used for graceful shutdown, not implemented.
 
     public static void main(String[] args) {
@@ -42,14 +46,6 @@ public class Starter {
 
     private void start() {
         try {
-            prometheusExecutor = Executors.newSingleThreadExecutor();
-            prometheusShutdownKey = new BigInteger(130, new SecureRandom()).toString(32);
-            logger.info("Prometheus on port {} with shutdownKey: '{}'", prometheusPort, prometheusShutdownKey);
-            prometheusExecutor.submit(new PrometheusRunnable(prometheusShutdownKey, prometheusPort));
-        } catch (Exception e) {
-            logger.error(e.getMessage(), e);
-        }
-        try {
             kclWorkerExecutor = Executors.newCachedThreadPool();
             kclWorker = new Worker.Builder().recordProcessorFactory(
                     new SampleRecordProcessor.SampleRecordProcessorFactory()
@@ -57,6 +53,11 @@ public class Starter {
             logger.info("kclWorker constructed successfully.");
             kclWorkerExecutor.submit(kclWorker);
             logger.info("kclWorker submitted to kclWorkerExecutor.");
+
+            prometheusExecutor = Executors.newSingleThreadExecutor();
+            prometheusShutdownKey = new BigInteger(130, new SecureRandom()).toString(32);
+            logger.info("Prometheus on port {} with shutdownKey: '{}'", prometheusPort, prometheusShutdownKey);
+            prometheusExecutor.submit(new PrometheusRunnable(prometheusShutdownKey, prometheusPort));
         } catch (Exception e) {
             logger.error(e.getMessage(), e);
         }
@@ -80,6 +81,7 @@ public class Starter {
 
         try (InputStream configStream = new FileInputStream(configFile)) {
             properties.load(configStream);
+            prometheusPort = Integer.parseInt(properties.getProperty("prometheusPort", "8090"));
         } catch (Exception e) {
             String msg = "Could not load properties file -DCONFIG_FILE from classpath";
             throw new IllegalStateException(msg, e);
@@ -87,7 +89,7 @@ public class Starter {
 
         String appName = properties.getProperty("appName");
         String kinesisInputStream = properties.getProperty("kinesisInputStream");
-        String workerID = properties.getProperty("workerID");
+        String workerId = properties.getProperty("workerId"); // Previously "workerID" with capital 'D'.
         String accessKey = properties.getProperty("accessKey");
         String secretKey = properties.getProperty("secretKey");
 
@@ -103,11 +105,19 @@ public class Starter {
             }
         };
 
-        KinesisClientLibConfiguration config = new KinesisClientLibConfiguration(appName, kinesisInputStream, credentialsProvider, workerID);
+        KinesisClientLibConfiguration config = new KinesisClientLibConfiguration(appName, kinesisInputStream, credentialsProvider, workerId);
         try {
             config.withRegionName(properties.getProperty("regionName"));
             config.withKinesisEndpoint(properties.getProperty("kinesisEndpoint"));
             config.withIdleTimeBetweenReadsInMillis(Long.parseLong(properties.getProperty("idleTimeBetweenReadsInMillis", "1000")));
+
+            Set<String> metricsEnabledDimensions = ImmutableSet.<String>builder()
+                    .addAll(KinesisClientLibConfiguration.METRICS_ALWAYS_ENABLED_DIMENSIONS)
+                    .add(MetricsHelper.SHARD_ID_DIMENSION_NAME)
+                    .add(LeaseCoordinator.WORKER_IDENTIFIER_METRIC)
+                    .build();
+
+            config.withMetricsEnabledDimensions(metricsEnabledDimensions);
         } catch (Exception e) {
             logger.error(e.getMessage(), e);
         }
