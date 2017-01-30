4b79a70d2ec175fea8a22b0c99acb965babcaef0
az3 <azureel@gmail.com>
2017-01-30 16:08:59 +0300

    Changed default prometheus port.

--- src/main/resources/prometheus.sh
+++ src/main/resources/prometheus.sh
@@ -1,6 +1,6 @@
 #/usr/bin/env bash
 while true; do
     echo `date` >>prometheus.log
-    curl -s localhost:8090/metrics >>prometheus.log
+    curl -s localhost:9751/metrics | grep kinesis_consumer_approximate_arrival_time_diff_seconds >>prometheus.log
     sleep 60
 done
